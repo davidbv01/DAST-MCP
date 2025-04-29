cf5852581332af03f4dca85ae2de161f6ade77d0
David Borrego Valenzuela <davidborregomdl@gmail.com>
2025-04-29 13:18:45 +0200

    Small changes in logging.

--- backend/config/logs_config.py
+++ backend/config/logs_config.py
@@ -1,20 +1,29 @@
 import logging
+import os
 
-# Configuración básica del logger
+# Basic logger setup
 def setup_logger():
-    # Crear el logger
+    # Create the .logs directory if it doesn't exist
+    log_dir = ".logs"
+    if not os.path.exists(log_dir):
+        os.makedirs(log_dir)
+
+    # Create the log file inside the .logs directory
+    log_file = os.path.join(log_dir, 'zap_scan.log')
+
+    # Create the logger
     logger = logging.getLogger("zap_logger")
     logger.setLevel(logging.INFO)
 
-    # Crear un manejador de archivo que escribe los logs en un archivo
-    file_handler = logging.FileHandler('zap_scan.log')
+    # Create a file handler that writes the logs to the file
+    file_handler = logging.FileHandler(log_file)
     file_handler.setLevel(logging.INFO)
 
-    # Crear un formato para los logs
+    # Create a format for the logs
     formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
     file_handler.setFormatter(formatter)
 
-    # Agregar el manejador al logger
+    # Add the handler to the logger
     logger.addHandler(file_handler)
 
     return logger
--- backend/services/zap_service.py
+++ backend/services/zap_service.py
@@ -1,8 +1,11 @@
 from zapv2 import ZAPv2
-import logging
+from config.logs_config import setup_logger
 import asyncio
 from dotenv import load_dotenv
 import os
+
+# Setup logger
+logger = setup_logger()
 
 # Load environment variables
 load_dotenv()
@@ -11,30 +14,30 @@ apiKey = os.getenv("ZAP_API_KEY")
 
 # Initialize ZAP API
 zap = ZAPv2(apikey=apiKey)
-    
+
 async def run_zap_full_scan(target_url):
-    logging.info("[*] Starting traditional Spider...")
+    logger.info("[*] Starting traditional Spider...")
     spider_id = zap.spider.scan(target_url)
     while int(zap.spider.status(spider_id)) < 100:
         await asyncio.sleep(2)
-    logging.info("[*] Spider completed!")
+    logger.info("[*] Spider completed!")
 
-    logging.info("[*] Starting AJAX Spider...")
+    logger.info("[*] Starting AJAX Spider...")
     zap.ajaxSpider.scan(target_url)
     while zap.ajaxSpider.status == 'running':
         await asyncio.sleep(2)
-    logging.info("[*] AJAX Spider completed!")
+    logger.info("[*] AJAX Spider completed!")
 
-    logging.info("[*] Starting Active Scan...")
+    logger.info("[*] Starting Active Scan...")
     active_scan_id = zap.ascan.scan(target_url)
     while int(zap.ascan.status(active_scan_id)) < 100:
         await asyncio.sleep(5)
-    logging.info("[*] Active Scan completed!")
+    logger.info("[*] Active Scan completed!")
 
-    logging.info("[*] Full scan completed successfully!")
+    logger.info("[*] Full scan completed successfully!")
 
-    logging.info("[*] Creating report")
+    logger.info("[*] Creating report")
     report_xml = zap.core.xmlreport()
     with open('zap_report.xml', 'w') as f:
         f.write(report_xml)
-    logging.info("[*] Report saved!")
+    logger.info("[*] Report saved!")
